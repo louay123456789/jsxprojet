9b30f8327f5d82f1db6ea01a5f4928bf0b01953b
louay <loupproduction7050@gmail.com>
2023-11-13 11:48:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Name from './Name'
+import Price from './Price'
+import Description from './Description'
+import Image from './Image'
+const name = "";
+const hasName = Boolean(name);
 
-function App() {
+function TextExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title><Name /></Card.Title>
+        <Card.Subtitle className="mb-2 text-muted"><Price/></Card.Subtitle>
+        <Card.Text><Description /></Card.Text>
+      </Card.Body>
+      <Image/>
+
+      
+      <div>
+      <p>Hello, {name ? name : "there"}!</p>
+    </div>   
+     <div>
+      {hasName ? (
+        <img src="aa.jpg" alt="Profile" />
+      ) : (
+        <p>No image available</p>
+      )}
+      <Card />
     </div>
-  );
+    
+    </Card>
+    
+    
+  )
 }
 
-export default App;
+
+export default TextExample;
+
